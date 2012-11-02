7fd7d7817aa48072e51629ac97c0be01aed23e29
Robert Stehwien <rstehwien@gmail.com>
2012-11-02 12:39:23 -0600

    fork each simulator to use processors

--- RunSimulator.rb
+++ RunSimulator.rb
@@ -1,69 +1,33 @@
 #!/usr/bin/ruby
 require "./SimulatorLib.rb"
 
-attacker = Character.new(
-{
-	:name => "Atacker",
-	:attack => nil,
-	:defense => nil,
-	:initiative => 0
-})
+def fork_sim(name, attack)
+	fork {
+		attacker = Character.new(
+		{
+			:name => "Atacker",
+			:attack => attack,
+			:defense => nil,
+			:initiative => 0
+		})
 
-defender = Character.new(
-{
-	:name => "Defender",
-	:attack => nil,
-	:defense => Defense.new,
-	:initiative => 0
-})
+		defender = Character.new(
+		{
+			:name => "Defender",
+			:attack => nil,
+			:defense => Defense.new,
+			:initiative => 0
+		})
 
-puts "=========="
-puts "BASIC DAMAGE"
-attacker.attack = Attack::create_damage
-puts CombatSimulator.new({ 
-	:character1 => attacker, 
-	:character2 => defender
-	}).run.format_stats
-puts "=========="
+		result = CombatSimulator.new({ :character1 => attacker, :character2 => defender }).run.format_stats
+		puts "==========\n#{name}\n#{result}=========="
+	}
+end
 
-
-puts "=========="
-puts "BASIC AFFLICTION"
-attacker.attack = Attack::create_affliction
-puts CombatSimulator.new({ 
-	:character1 => attacker, 
-	:character2 => defender
-	}).run.format_stats
-puts "=========="
-
-puts "=========="
-puts "CUMULATIVE AFFLICTION"
-attacker.attack = Attack::create_affliction({:cumulative_statuses=>[1,2]})
-puts CombatSimulator.new({ 
-	:character1 => attacker, 
-	:character2 => defender
-	}).run.format_stats
-puts "=========="
-
-puts "=========="
-puts "PROGRESSIVE AFFLICTION"
-attacker.attack = Attack::create_affliction({:is_progressive=>true})
-puts CombatSimulator.new({ 
-	:character1 => attacker, 
-	:character2 => defender
-	}).run.format_stats
-puts "=========="
-
-puts "=========="
-puts "CUMULATIVE+PROGRESSIVE AFFLICTION"
-attacker.attack = Attack::create_affliction(
-	{
-		:cumulative_statuses=>[1,2],
-		:is_progressive=>true
-	})
-puts CombatSimulator.new({ 
-	:character1 => attacker, 
-	:character2 => defender
-	}).run.format_stats
-puts "=========="
-
+fork_sim("BASIC DAMAGE", Attack::create_damage)
+fork_sim("BASIC AFFLICTION", Attack::create_affliction)
+fork_sim("CUMULATIVE AFFLICTION", Attack::create_affliction({:cumulative_statuses=>[1,2]}))
+fork_sim("PROGRESSIVE AFFLICTION", Attack::create_affliction({:is_progressive=>true}))
+fork_sim("CUMULATIVE+PROGRESSIVE AFFLICTION", 
+	Attack::create_affliction({ :cumulative_statuses=>[1,2], :is_progressive=>true }))
+Process.waitall
