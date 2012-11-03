e00c165d06e462e544b53f94faf21e2307c685f0
Robert Stehwien <rstehwien@gmail.com>
2012-11-03 08:45:40 -0600

    use threads because fork fails on windows

--- RunSimulator.rb
+++ RunSimulator.rb
@@ -1,20 +1,22 @@
 #!/usr/bin/ruby
 require "./SimulatorLib.rb"
 
-def fork_sim(name, attack)
-	fork {
-		attacker = Character.new({:name => "Atacker", :attack => attack})
-		defender = Character.new({:name => "Defender", :defense => Defense.new})
-
-		result = CombatSimulator.new({ :character1 => attacker, :character2 => defender }).run.format_stats
-		puts "==========\n#{name}\n#{result}=========="
-	}
+def run_sim(name, attack)
+	Thread.new {
+			attacker = Character.new({:name => "Atacker", :attack => attack})
+			defender = Character.new({:name => "Defender", :defense => Defense.new})
+	
+			result = CombatSimulator.new({ :character1 => attacker, :character2 => defender }).run.format_stats
+			puts "==========\n#{name}\n#{result}=========="
+		}
 end
 
-fork_sim("BASIC DAMAGE", Attack::create_damage)
-#fork_sim("BASIC AFFLICTION", Attack::create_affliction)
-fork_sim("CUMULATIVE AFFLICTION", Attack::create_affliction({:cumulative_statuses=>[1,2]}))
-fork_sim("PROGRESSIVE AFFLICTION", Attack::create_affliction({:is_progressive=>true}))
-fork_sim("CUMULATIVE+PROGRESSIVE AFFLICTION", 
+run_sim("BASIC DAMAGE", Attack::create_damage)
+#run_sim("BASIC AFFLICTION", Attack::create_affliction)
+run_sim("CUMULATIVE AFFLICTION", Attack::create_affliction({:cumulative_statuses=>[1,2]}))
+run_sim("PROGRESSIVE AFFLICTION", Attack::create_affliction({:is_progressive=>true}))
+run_sim("CUMULATIVE+PROGRESSIVE AFFLICTION", 
 	Attack::create_affliction({ :cumulative_statuses=>[1,2], :is_progressive=>true }))
-Process.waitall
+
+# Wait for all threads to end
+Thread.list.each {|t| t.join if t != Thread.current }
